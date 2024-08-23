d5dcc17f43cbf7d2224c7ad8fefce94a44fe05ee
Yuşa Çapraz <yusacapraz@protonmail.com>
2024-08-24 01:41:48 +0300

    Spring application moved to main package

--- src/main/java/com/cyberfreak/service/main/ContentServiceApplication.java
+++ src/main/java/com/cyberfreak/service/main/ContentServiceApplication.java
@@ -1,4 +1,4 @@
-package com.cyberfreak.service;
+package com.cyberfreak.service.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
