23ddb1305f98195791460b2c458dfba54a0e679c
Yuşa Çapraz <yusacapraz@protonmail.com>
2024-08-24 00:21:23 +0300

    Auditable entity listener parameter validators added

--- src/main/java/com/cyberfreak/service/domain/listeners/AuditableEntityListener.java
+++ src/main/java/com/cyberfreak/service/domain/listeners/AuditableEntityListener.java
@@ -4,25 +4,26 @@ import com.cyberfreak.service.domain.base.AuditableEntity;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreRemove;
 import jakarta.persistence.PreUpdate;
+import org.jetbrains.annotations.NotNull;
 
 import java.time.LocalDateTime;
 
 public class AuditableEntityListener {
 
     @PrePersist
-    public void prePersist(AuditableEntity entity) {
+    public void prePersist(@NotNull AuditableEntity entity) {
         LocalDateTime now = LocalDateTime.now();
         entity.setCreationTime(now);
         entity.setModificationTime(now);
     }
 
     @PreUpdate
-    public void preUpdate(AuditableEntity entity) {
+    public void preUpdate(@NotNull AuditableEntity entity) {
         entity.setModificationTime(LocalDateTime.now());
     }
 
     @PreRemove
-    public void preRemove(AuditableEntity entity) {
-        entity.setDeletedTime(LocalDateTime.now());
+    public void preRemove(@NotNull AuditableEntity entity) {
+        entity.setDeletionTime(LocalDateTime.now());
     }
 }
