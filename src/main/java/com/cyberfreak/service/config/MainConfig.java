8036e33f820e8c06ffd34e68f263e6999408856e
Yuşa Çapraz <yusacapraz@protonmail.com>
2024-08-24 18:49:32 +0300

    Entity scan declared within main config bean

---
+++ src/main/java/com/cyberfreak/service/config/MainConfig.java
@@ -0,0 +1,9 @@
+package com.cyberfreak.service.config;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EntityScan(basePackages = "com.cyberfreak.service.domain")
+public class MainConfig {
+}
