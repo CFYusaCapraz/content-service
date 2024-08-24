5f2358476483a10ffabb071a403d68feca512d48
Yuşa Çapraz <yusacapraz@protonmail.com>
2024-08-24 15:17:28 +0300

    Implemented toDto (entity2dto, request2dto), fromDto and partialUpdate methods

--- src/main/java/com/cyberfreak/service/mapper/PageContentMapper.java
+++ src/main/java/com/cyberfreak/service/mapper/PageContentMapper.java
@@ -1,13 +1,33 @@
 package com.cyberfreak.service.mapper;
 
 import com.cyberfreak.service.api.request.CreatePageContentRequest;
+import com.cyberfreak.service.domain.PageContent;
 import com.cyberfreak.service.dto.PageContentDto;
-import org.mapstruct.Mapper;
-import org.mapstruct.MappingConstants;
-import org.mapstruct.ReportingPolicy;
+import com.cyberfreak.service.service.ApplicationService;
+import org.jetbrains.annotations.NotNull;
+import org.mapstruct.*;
+import org.mapstruct.factory.Mappers;
 
-@Mapper(componentModel = MappingConstants.ComponentModel.SPRING, unmappedTargetPolicy = ReportingPolicy.IGNORE)
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING, unmappedTargetPolicy = ReportingPolicy.IGNORE,
+        uses = {ApplicationService.class})
 public interface PageContentMapper {
 
-    PageContentDto toDTO(CreatePageContentRequest createPageContentRequest);
+    PageContentMapper INSTANCE = Mappers.getMapper(PageContentMapper.class);
+
+    PageContent toEntity(PageContentDto pageContentDto);
+
+    @AfterMapping
+    default void linkContentItems(@MappingTarget @NotNull PageContent pageContent) {
+        pageContent.getContentItems().forEach(contentItem -> contentItem.setPage(pageContent));
+    }
+
+    PageContentDto toDto(PageContent pageContent);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    PageContent partialUpdate(PageContentDto pageContentDto, @MappingTarget PageContent pageContent);
+
+
+    @Mapping(source = "pageName", target = "pageName")
+    @Mapping(source = "applicationId", target = "application", qualifiedByName = "mapApplicationIdToApplicationDto")
+    PageContentDto toDto(CreatePageContentRequest createPageContentRequest);
 }
