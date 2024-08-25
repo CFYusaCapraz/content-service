52982b23d1a3d9bbe8b6389eb070e524e6b7fbec
Yuşa Çapraz <yusacapraz@protonmail.com>
2024-08-25 20:14:55 +0300

    Custom header "x-user-id" added
    All API calls require x-user-id header to work, because the audit information is recorded to db

--- src/main/java/com/cyberfreak/services/domain/listeners/AuditableEntityListener.java
+++ src/main/java/com/cyberfreak/services/domain/listeners/AuditableEntityListener.java
@@ -1,5 +1,6 @@
 package com.cyberfreak.services.domain.listeners;
 
+import com.cyberfreak.services.api.context.UserContext;
 import com.cyberfreak.services.domain.base.AuditableEntity;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreRemove;
@@ -11,19 +12,31 @@ import java.time.LocalDateTime;
 public class AuditableEntityListener {
 
     @PrePersist
-    public void prePersist(@NotNull AuditableEntity entity) {
-        LocalDateTime now = LocalDateTime.now();
-        entity.setCreationTime(now);
-        entity.setModificationTime(now);
+    public void prePersist(@NotNull AuditableEntity<?, ?> entity) {
+        String userId = UserContext.getCurrentUserId();
+        if (userId != null) {
+            entity.setCreatedBy(userId);
+            LocalDateTime now = LocalDateTime.now();
+            entity.setCreationTime(now);
+            entity.setModificationTime(now);
+        }
     }
 
     @PreUpdate
-    public void preUpdate(@NotNull AuditableEntity entity) {
-        entity.setModificationTime(LocalDateTime.now());
+    public void preUpdate(@NotNull AuditableEntity<?, ?> entity) {
+        String userId = UserContext.getCurrentUserId();
+        if (userId != null) {
+            entity.setModifiedBy(userId);
+            entity.setModificationTime(LocalDateTime.now());
+        }
     }
 
     @PreRemove
-    public void preRemove(@NotNull AuditableEntity entity) {
-        entity.setDeletionTime(LocalDateTime.now());
+    public void preRemove(@NotNull AuditableEntity<?, ?> entity) {
+        String userId = UserContext.getCurrentUserId();
+        if (userId != null) {
+            entity.setDeletedBy(userId);
+            entity.setDeletionTime(LocalDateTime.now());
+        }
     }
 }
