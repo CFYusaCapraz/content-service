c4a4a92b1b3eafffb1611ae29bb0a1fb6d64b68d
Yuşa Çapraz <yusacapraz@protonmail.com>
2024-08-25 17:05:21 +0300

    Get application by id API implemented

--- src/main/java/com/cyberfreak/services/api/controller/ApplicationController.java
+++ src/main/java/com/cyberfreak/services/api/controller/ApplicationController.java
@@ -1,12 +1,15 @@
 package com.cyberfreak.services.api.controller;
 
 import com.cyberfreak.services.api.constants.ApiEndpoints;
+import com.cyberfreak.services.api.constants.ApiPaths;
 import com.cyberfreak.services.api.response.ListResultResponse;
+import com.cyberfreak.services.api.response.SingleResultResponse;
 import com.cyberfreak.services.dto.ApplicationDto;
 import com.cyberfreak.services.service.ApplicationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +27,11 @@ public class ApplicationController {
     public ListResultResponse<ApplicationDto> getApplications() {
         List<ApplicationDto> applicationDtoList = applicationService.getApplications();
         return new ListResultResponse<>(applicationDtoList);
+    }
+
+    @GetMapping(path = ApiPaths.APPLICATION_BY_ID_PATH)
+    public SingleResultResponse<ApplicationDto> getApplication(@PathVariable(ApiPaths.APPLICATION_ID) Long id) {
+        ApplicationDto applicationDto = applicationService.getApplication(id);
+        return new SingleResultResponse<>(applicationDto);
     }
 }
