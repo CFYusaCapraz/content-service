ad47578a57d653c20b98cb711c4deea5514f31e2
Yuşa Çapraz <yusacapraz@protonmail.com>
2024-08-25 21:03:10 +0300

    All entities annotated with DynamicUpdate BaseEntity annotated with SoftDelete to apply soft delete instead of hard delete

--- src/main/java/com/cyberfreak/services/domain/base/BaseEntity.java
+++ src/main/java/com/cyberfreak/services/domain/base/BaseEntity.java
@@ -6,11 +6,14 @@ import com.cyberfreak.services.dto.base.BaseDto;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.SoftDelete;
+import org.hibernate.annotations.SoftDeleteType;
 
 @Getter
 @Setter
 @MappedSuperclass
 @EntityListeners(BaseEntityListener.class)
+@SoftDelete(columnName = "is_deleted", strategy = SoftDeleteType.DELETED)
 public abstract class BaseEntity<Entity extends BaseEntity<Entity, DTO>, DTO extends BaseDto> implements EntityMapper<Entity, DTO> {
 
     @Id
@@ -22,7 +25,7 @@ public abstract class BaseEntity<Entity extends BaseEntity<Entity, DTO>, DTO ext
     @Column(name = "version", nullable = false)
     protected Integer version;
 
-    @Column(name = "is_deleted", nullable = false)
+    @Column(name = "is_deleted", nullable = false, insertable = false, updatable = false)
     protected Boolean isDeleted = Boolean.FALSE;
 
     @Override
