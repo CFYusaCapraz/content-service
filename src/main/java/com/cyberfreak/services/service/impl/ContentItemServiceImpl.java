9349778abd099051adfde13356d17ed74ce34e55
Yuşa Çapraz <yusacapraz@protonmail.com>
2024-08-25 15:36:36 +0300

    Content item get service implemented

--- src/main/java/com/cyberfreak/services/service/impl/ContentItemServiceImpl.java
+++ src/main/java/com/cyberfreak/services/service/impl/ContentItemServiceImpl.java
@@ -1,6 +1,7 @@
 package com.cyberfreak.services.service.impl;
 
 import com.cyberfreak.services.dto.ContentItemDto;
+import com.cyberfreak.services.repository.ContentItemRepository;
 import com.cyberfreak.services.service.ContentItemService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,8 +12,11 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Slf4j
 public class ContentItemServiceImpl implements ContentItemService {
+
+    private final ContentItemRepository contentItemRepository;
+
     @Override
     public ContentItemDto getContentItem(@NotNull Long id) {
-        return null;
+        return contentItemRepository.findById(id).orElseThrow(() -> new RuntimeException("Content item not found")).toDto();
     }
 }
