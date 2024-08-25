695a574b4420748368499cf1e3fbd4130e766b54
Yuşa Çapraz <yusacapraz@protonmail.com>
2024-08-25 18:12:30 +0300

    Application controller added to swagger group Content group path match refactored

--- src/main/java/com/cyberfreak/services/config/SwaggerConfig.java
+++ src/main/java/com/cyberfreak/services/config/SwaggerConfig.java
@@ -12,6 +12,14 @@ public class SwaggerConfig {
         return GroupedOpenApi.builder()
                 .group("content-v1-content")
                 .displayName("Content API")
-                .pathsToMatch("/api/v1/*content*/**").build();
+                .pathsToMatch("/api/v1/*contents/**").build();
+    }
+
+    @Bean
+    public GroupedOpenApi applicationApi() {
+        return GroupedOpenApi.builder()
+                .group("content-v1-application")
+                .displayName("Application API")
+                .pathsToMatch("/api/v1/applications/**").build();
     }
 }
