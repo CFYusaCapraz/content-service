21a1822d06b6254f4b5c6e8658eb7b047bbbfc42
Yuşa Çapraz <yusacapraz@protonmail.com>
2024-08-30 00:40:39 +0300

    Unused import removed

--- src/main/java/com/cyberfreak/services/api/response/base/BaseResponse.java
+++ src/main/java/com/cyberfreak/services/api/response/base/BaseResponse.java
@@ -1,7 +1,6 @@
 package com.cyberfreak.services.api.response.base;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.Getter;
