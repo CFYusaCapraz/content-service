f92548f7899a9173d662557fb8d90330b9a0ef65
Yuşa Çapraz <yusacapraz@protonmail.com>
2024-08-30 01:32:19 +0300

    Create application request model swagger docs added

--- src/main/java/com/cyberfreak/services/api/request/application/CreateApplicationRequest.java
+++ src/main/java/com/cyberfreak/services/api/request/application/CreateApplicationRequest.java
@@ -1,5 +1,6 @@
 package com.cyberfreak.services.api.request.application;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -10,11 +11,16 @@ import java.io.Serializable;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(description = "Application table is needed for being able to differentiate contents of either " +
+        "same application name but different language or same language bu different application. " +
+        "Ex: Name = <project_name>IOS / Language = EN or Name = <project_name>Web / Language = RU")
 public class CreateApplicationRequest implements Serializable {
 
+    @Schema(description = "Name property of application. Must not be blank!")
     @NotBlank
     private String name;
 
+    @Schema(description = "Language property of application. Must not be blank!")
     @NotBlank
     private String language;
 }
