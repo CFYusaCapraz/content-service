1c4ebee71a18c0ede18c9faf6f30c3de337e7107
Yuşa Çapraz <yusacapraz@protonmail.com>
2024-08-30 00:50:32 +0300

    Validation annotations added application controller

--- src/main/java/com/cyberfreak/services/api/controller/ApplicationController.java
+++ src/main/java/com/cyberfreak/services/api/controller/ApplicationController.java
@@ -10,6 +10,10 @@ import com.cyberfreak.services.api.response.base.SaveEntityResponse;
 import com.cyberfreak.services.api.response.base.SingleResultResponse;
 import com.cyberfreak.services.dto.ApplicationDto;
 import com.cyberfreak.services.service.ApplicationService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -31,33 +35,38 @@ public class ApplicationController {
     }
 
     @GetMapping(path = ApiPaths.APPLICATION_ID_PATH)
-    public SingleResultResponse<ApplicationDto> getApplication(@PathVariable(ApiPaths.APPLICATION_ID) Long id) {
+    public SingleResultResponse<ApplicationDto> getApplication(
+            @NotNull @Positive @PathVariable(ApiPaths.APPLICATION_ID) Long id) {
         ApplicationDto applicationDto = applicationService.getApplication(id);
         return new SingleResultResponse<>(applicationDto);
     }
 
     @GetMapping(path = ApiPaths.APPLICATION_SEARCH_PATH)
-    public ListResultResponse<ApplicationDto> getApplicationsByNameAndLanguage(@RequestParam(ApiPaths.APPLICATION_NAME) String name,
-                                                                               @RequestParam(ApiPaths.APPLICATION_LANGUAGE) String language) {
+    public ListResultResponse<ApplicationDto> getApplicationsByNameAndLanguage(
+            @NotBlank @RequestParam(ApiPaths.APPLICATION_NAME) String name,
+            @NotBlank @RequestParam(ApiPaths.APPLICATION_LANGUAGE) String language) {
         List<ApplicationDto> applicationDtoList = applicationService.getApplicationsByNameAndLanguage(name, language);
         return new ListResultResponse<>(applicationDtoList);
     }
 
     @PostMapping
-    public SaveEntityResponse createApplication(@RequestBody CreateApplicationRequest createApplicationRequest) {
+    public SaveEntityResponse createApplication(
+            @Valid @RequestBody CreateApplicationRequest createApplicationRequest) {
         ApplicationDto applicationDto = applicationService.createApplication(createApplicationRequest);
         return new SaveEntityResponse(applicationDto.getId());
     }
 
     @PutMapping(path = ApiPaths.APPLICATION_ID_PATH)
-    public SaveEntityResponse updateApplication(@PathVariable(ApiPaths.APPLICATION_ID) Long id,
-                                                @RequestBody UpdateApplicationRequest updateApplicationRequest) {
+    public SaveEntityResponse updateApplication(
+            @NotNull @Positive @PathVariable(ApiPaths.APPLICATION_ID) Long id,
+            @Valid @RequestBody UpdateApplicationRequest updateApplicationRequest) {
         ApplicationDto applicationDto = applicationService.updateApplication(id, updateApplicationRequest);
         return new SaveEntityResponse(applicationDto.getId());
     }
 
     @DeleteMapping(path = ApiPaths.APPLICATION_ID_PATH)
-    public BaseResponse deleteApplication(@PathVariable(ApiPaths.APPLICATION_ID) Long id) {
+    public BaseResponse deleteApplication(
+            @NotNull @Positive @PathVariable(ApiPaths.APPLICATION_ID) Long id) {
         applicationService.deleteApplication(id);
         return new BaseResponse(true);
     }
