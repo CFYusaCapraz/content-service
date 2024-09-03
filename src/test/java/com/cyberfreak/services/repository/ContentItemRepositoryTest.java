145bfce8162ead3baceae89b782a805250caad90
Yuşa Çapraz <yusacapraz@protonmail.com>
2024-09-04 02:04:28 +0300

    Content item repository test class created

---
+++ src/test/java/com/cyberfreak/services/repository/ContentItemRepositoryTest.java
@@ -0,0 +1,27 @@
+package com.cyberfreak.services.repository;
+
+import com.cyberfreak.services.config.MainConfig;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@DataJpaTest
+@ContextConfiguration(classes = {MainConfig.class})
+@ActiveProfiles(profiles = "base_test")
+@ExtendWith(SpringExtension.class)
+class ContentItemRepositoryTest {
+
+    @Autowired
+    private ContentItemRepository contentItemRepository;
+
+    @Test
+    void test() {
+        assertTrue(true);
+    }
+}
