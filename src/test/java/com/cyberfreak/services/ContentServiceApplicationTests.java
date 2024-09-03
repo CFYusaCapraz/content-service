6e70a0d176cfd2302360d01610403ac173b55c58
Yuşa Çapraz <yusacapraz@protonmail.com>
2024-09-04 00:44:23 +0300

    Spring test profile created

--- src/test/java/com/cyberfreak/services/ContentServiceApplicationTests.java
+++ src/test/java/com/cyberfreak/services/ContentServiceApplicationTests.java
@@ -1,10 +1,13 @@
 package com.cyberfreak.services;
 
+import com.cyberfreak.services.main.ContentServiceApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
-class ContentServiceApplicationTests {
+@SpringBootTest(classes = ContentServiceApplication.class)
+@ActiveProfiles(profiles = "base_test")
+public class ContentServiceApplicationTests {
 
     @Test
     void contextLoads() {
